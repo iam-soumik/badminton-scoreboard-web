e0037b23949e405e72bfac34f96f0e377b14b948
Soumik Maity <soumik.mca.job@gmail.com>
2026-02-16 18:17:26 +0530

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// src/firebase.js
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsxysJqVUaLcXJtCaWloI2QgdHnmgHg_E",
+  authDomain: "badminton-tournament-nasibpur.firebaseapp.com",
+  projectId: "badminton-tournament-nasibpur",
+  storageBucket: "badminton-tournament-nasibpur.firebasestorage.app",
+  messagingSenderId: "892926096864",
+  appId: "1:892926096864:web:1ff78d81a39555b4b06785"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
